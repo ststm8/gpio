dee47b188611f4d3452e970ad5ecd9aae31c4823
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-01-16 14:08:59 +0200

    sync

---
+++ common.h
@@ -0,0 +1,20 @@
+#ifndef __sstm8_common__
+#define __sstm8_common__
+
+#ifndef __STM8_HEADERS__
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+// --
+
+#include "gpio.h"
+
+// --
+#ifdef __cplusplus
+  }
+#endif
+
+#endif
+
+#endif
---
+++ stm8-gpio.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __ststm8_gpio__
+#define __ststm8_gpio__
+
+void cfgPinIn();
+void cfgPinOut();
+
+void setPin();
+void getPin();
+
+#endif
